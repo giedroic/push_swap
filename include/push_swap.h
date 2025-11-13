7717eee7b84924da97064c4a7bda2664f1680e31
Aleksander Giedroić <150075381+giedroic@users.noreply.github.com>
2025-11-13 17:43:49 +0000

    Add bool library

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,13 +6,14 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 10:22:09 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/11/13 17:07:45 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/11/13 17:43:39 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include "libft.h"
+# include <stdbool.h>
 # include <limits.h>
 
 #endif
