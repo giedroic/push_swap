e8940bc4a10ab3d3a4e3cb79addd506cb3578c5f
Aleksander Giedroić <150075381+giedroic@users.noreply.github.com>
2025-11-13 17:08:03 +0000

    Include limits library

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,12 +6,13 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 10:22:09 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/11/13 17:07:17 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/11/13 17:07:45 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include "libft.h"
+# include <limits.h>
 
 #endif
