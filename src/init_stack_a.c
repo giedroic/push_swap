4d66e69941662c2c7a75087a0344ef53e9614b36
Aleksander Giedroić <150075381+giedroic@users.noreply.github.com>
2025-11-15 23:00:19 +0000

    Pass stack_a to is_duplicate by address

--- src/init_stack_a.c
+++ src/init_stack_a.c
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 19:19:20 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/11/15 22:59:19 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/11/15 22:59:53 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ void	init_stack_a(t_stack_node **a, char *argv[])
 		n = ft_atol(argv[i]);
 		if (n > INT_MAX || n < INT_MIN)
 			error_handler();
-		is_duplicate(a, n);
+		is_duplicate(&a, n);
 		append_node(a, (int) n); // TO DO
 		++i;
 	}
