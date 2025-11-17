00589c73c82c8e2743bce64ca2d84fc4d8ab2404
Aleksander Giedroić <150075381+giedroic@users.noreply.github.com>
2025-11-17 20:27:27 +0100

    Add rotate both function

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,22 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 20:07:57 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/11/17 20:21:21 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/11/17 20:27:15 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	rotate_both(t_stack_node **a,
+				t_stack_node **b,
+				t_stack_node *cheapest_node)
+{
+	while (*a != cheapest_node->target_node
+		&& *b != cheapest_node)
+		rr(a, b);
+	set_current_position(*a);
+	set_current_position(*b);
+}
 
 void	finish_rotation(t_stack_node **stack,
 			t_stack_node *top_node,
