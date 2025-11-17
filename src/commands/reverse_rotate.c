b2cdc8eabf891887979d466448e516da23fdcffb
Aleksander Giedroić <150075381+giedroic@users.noreply.github.com>
2025-11-17 16:35:06 +0100

    Add reverse_rotate function

---
+++ src/commands/reverse_rotate.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 16:25:57 by agiedroi          #+#    #+#             */
+/*   Updated: 2025/11/17 16:34:51 by agiedroi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	reverse_rotate(t_stack_node **stack)
+{
+	t_stack_node	*last;
+
+	if (stack == NULL || *stack == NULL || stack_len(*stack) == 1)
+		return ;
+	last = find_last_node(*stack);
+	last->prev->next = NULL;
+	last->next = *stack;
+	last->prev = NULL;
+	*stack = last;
+	last->next->prev = last;
+}
