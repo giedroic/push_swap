4b6444ea3358ced8ac1b5773d22241d16f3603a8
Aleksander Giedroić <150075381+giedroic@users.noreply.github.com>
2025-11-17 18:54:53 +0100

    Add function set_target_node and set_current_position

---
+++ src/push_swap_init.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_init.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 18:27:13 by agiedroi          #+#    #+#             */
+/*   Updated: 2025/11/17 18:53:57 by agiedroi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void set_target_node(t_stack_node *a, t_stack_node *b)
+{
+	t_stack_node	*current_a;
+	t_stack_node	*target_node;
+	long		best_match_index;
+
+	while (b != NULL)
+	{
+		best_match_index = LONG_MAX;
+		current_a = a;
+		while (current_a != NULL)
+		{
+			if (current_a->value > b->value
+				&& current_a->value < best_match_index)
+			{
+				best_match_index = current_a->value;
+				target_node = current_a;
+			}
+		}
+		if (LONG_MAX == best_match_index)
+			b->target_node = find_smallest(a);
+		else
+			b->target_node = target_node;
+		b = b->next;
+	}
+}
+
+void	set_current_position(t_stack_node *stack)
+{
+	int	i;
+	int	centerline;
+
+	i = 0;
+	if (stack == NULL)
+		return ;
+	centerline = stack_len(stack) / 2;
+	while (stack != NULL)
+	{
+		stack->current_position = i;
+		if (i <= centerline)
+			stack->above_median = true;
+		else
+			stack->above_median = false;
+		stack = stack->next;
+		++i;	
+	}
+}
