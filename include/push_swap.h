46cf9cc6ef795835c65b3998ed41af5471e0498b
Aleksander Giedroić <150075381+giedroic@users.noreply.github.com>
2025-11-17 11:32:09 +0100

    Add missing semicolon

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 09:03:13 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/11/17 11:31:01 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/11/17 11:32:00 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,5 @@ void	ft_free_split(char **split_array);
 t_stack_node	*find_last(t_stack_node *stack);
 void	append_node(t_stack_node **stack, int n);
 
-void	stack_init(t_stack_node **a, char **argv, bool is_argc_2)
+void	stack_init(t_stack_node **a, char **argv, bool is_argc_2);
 #endif
