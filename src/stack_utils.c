cd41519471c4182856691c97915b421308340e33
Aleksander Giedroić <150075381+giedroic@users.noreply.github.com>
2025-11-17 11:15:13 +0100

    Add find_last function

---
+++ src/stack_utils.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 11:12:06 by agiedroi          #+#    #+#             */
+/*   Updated: 2025/11/17 11:14:52 by agiedroi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack_node	*find_last(t_stack_node *stack)
+{
+	if (stack == NULL)
+		return (NULL);
+	while (stack->next != NULL)
+		stack = stack->next;
+	return (stack);
+}
+
+
