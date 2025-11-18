f0138a01e0893900e496c9a0aafd8a5ae3c395d8
Aleksander Giedroic <agiedroi@c2r1s1.42warsaw.pl>
2025-11-18 15:06:13 +0100

    Fix norm

--- include/push_swap.h
+++ include/push_swap.h
@@ -1,11 +1,12 @@
-/* ************************************************************************** */ /*                                                                            */
+/* ************************************************************************** */
+/*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 09:03:13 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/11/17 14:38:35 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/11/18 15:06:04 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,45 +32,45 @@ typedef struct s_stack_node
 	struct s_stack_node	*prev;
 }	t_stack_node;
 
-int		error_duplicate(t_stack_node *a, int n);
-void		free_stack(t_stack_node **stack);
-void		ft_free_split(char **split_array);
-void		error_free(t_stack_node **a, char **argv, bool is_argc_2);
+int				error_duplicate(t_stack_node *a, int n);
+void			free_stack(t_stack_node **stack);
+void			ft_free_split(char **split_array);
+void			error_free(t_stack_node **a, char **argv, bool is_argc_2);
 
 t_stack_node	*find_last_node(t_stack_node *stack);
-void		append_node(t_stack_node **stack, int n);
-int	stack_len(t_stack_node *stack);
+void			append_node(t_stack_node **stack, int n);
+int				stack_len(t_stack_node *stack);
 t_stack_node	*find_smallest(t_stack_node *stack);
 t_stack_node	*find_cheapest(t_stack_node *stack);
 
-void		stack_init(t_stack_node **a, char **argv, bool is_argc_2);
+void			stack_init(t_stack_node **a, char **argv, bool is_argc_2);
 
-bool	stack_sorted(t_stack_node *stack);
-void	sort_three(t_stack_node **a);
-void	handle_five(t_stack_node **a, t_stack_node **b);
+bool			stack_sorted(t_stack_node *stack);
+void			sort_three(t_stack_node **a);
+void			handle_five(t_stack_node **a, t_stack_node **b);
 
-void	sa(t_stack_node **a);
-void	sb(t_stack_node **b);
-void	ss(t_stack_node **a, t_stack_node **b);
-void	pa(t_stack_node **a, t_stack_node **b);
-void	pb(t_stack_node **b, t_stack_node **a);
-void	ra(t_stack_node **a);
-void	rb(t_stack_node **b);
-void	rr(t_stack_node **a, t_stack_node **b);
-void	rra(t_stack_node **a);
-void	rrb(t_stack_node **b);
-void	rrr(t_stack_node **a, t_stack_node **b);
+void			sa(t_stack_node **a);
+void			sb(t_stack_node **b);
+void			ss(t_stack_node **a, t_stack_node **b);
+void			pa(t_stack_node **a, t_stack_node **b);
+void			pb(t_stack_node **b, t_stack_node **a);
+void			ra(t_stack_node **a);
+void			rb(t_stack_node **b);
+void			rr(t_stack_node **a, t_stack_node **b);
+void			rra(t_stack_node **a);
+void			rrb(t_stack_node **b);
+void			rrr(t_stack_node **a, t_stack_node **b);
 
-void	set_current_position(t_stack_node *stack);
-void	set_cost(t_stack_node *a, t_stack_node *b);
-void	set_cheapest(t_stack_node *b);
-void	init_nodes(t_stack_node *a, t_stack_node *b);
+void			set_current_position(t_stack_node *stack);
+void			set_cost(t_stack_node *a, t_stack_node *b);
+void			set_cheapest(t_stack_node *b);
+void			init_nodes(t_stack_node *a, t_stack_node *b);
 
-void	finish_rotation(t_stack_node **stack,
-			t_stack_node *top_node,
-			char stack_name);
-void	average_partitioning(t_stack_node **a, t_stack_node **b);
-void	push_till_three(t_stack_node **a, t_stack_node **b);
+void			average_partitioning(t_stack_node **a, t_stack_node **b);
+void			push_till_three(t_stack_node **a, t_stack_node **b);
 
-void	push_swap(t_stack_node **a, t_stack_node **b);
+void			finish_rotation(t_stack_node **stack,
+					t_stack_node *top_node,
+					char stack_name);
+void			push_swap(t_stack_node **a, t_stack_node **b);
 #endif
